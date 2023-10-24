8b541bcdcd20a2a095e8e7204fce0cd9d24b13d2
fukicycle <lego.sato.4135@gmail.com>
2023-10-23 21:32:22 -0700

    単語の登録されていないLevelを表示しないように修正

--- jashter/src/main/kotlin/jp/fukicycle/jashter/api/repsitory/IMeaningOfWordRepository.kt
+++ jashter/src/main/kotlin/jp/fukicycle/jashter/api/repsitory/IMeaningOfWordRepository.kt
@@ -1,11 +1,11 @@
 package jp.fukicycle.jashter.api.repsitory
 
 import jp.fukicycle.jashter.api.model.MeaningOfWord
-import jp.fukicycle.jashter.api.model.PartOfSpeech
 import org.springframework.data.repository.CrudRepository
 import org.springframework.stereotype.Repository
 
 @Repository
 interface IMeaningOfWordRepository : CrudRepository<MeaningOfWord, Long> {
     fun findAllByLevelIdAndPartOfSpeechId(levelId: Long, partOfSpeechId: Long): List<MeaningOfWord>
+    fun findAllByLevelId(levelId: Long): List<MeaningOfWord>
 }
--- jashter/src/main/kotlin/jp/fukicycle/jashter/api/service/LevelService.kt
+++ jashter/src/main/kotlin/jp/fukicycle/jashter/api/service/LevelService.kt
@@ -2,6 +2,7 @@ package jp.fukicycle.jashter.api.service
 
 import jp.fukicycle.jashter.api.dto.response.LevelResponseDto
 import jp.fukicycle.jashter.api.repsitory.ILevelRepository
+import jp.fukicycle.jashter.api.repsitory.IMeaningOfWordRepository
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.stereotype.Service
 
@@ -9,11 +10,16 @@ import org.springframework.stereotype.Service
 class LevelService : ILevelService {
     @Autowired
     lateinit var levelRepository: ILevelRepository
+
+    @Autowired
+    lateinit var meaningOfWordRepository: IMeaningOfWordRepository
     override fun get(): List<LevelResponseDto> {
         val items = mutableListOf<LevelResponseDto>()
         val levels = levelRepository.findAll()
         for (level in levels) {
-            items.add(LevelResponseDto(level.id, level.name))
+            if (meaningOfWordRepository.findAllByLevelId(level.id).isNotEmpty()) {
+                items.add(LevelResponseDto(level.id, level.name))
+            }
         }
         return items
     }
